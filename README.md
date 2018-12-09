53f450c490937e1c4ebf7da6cd010dd341539560
Justin Gaskins <gaskij@rpi.edu>
2018-12-09 15:40:27 -0500

    Added register printing and file input handling

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # cs2500-mips-simulator
 Group project for Computer Organization CSCI2500 - Pipelined MIPS Simulator - Fall 2018
+Names:
+> Priya Sapra
+> Justin Gaskins
+> Bruno Kaufmann
 
 Run `> python3 p1.py [mode] [input]`
 
--- p1.py
+++ p1.py
@@ -17,16 +17,22 @@ if __name__ == "__main__":
 
 		# open and parse file
 		file = open(sys.argv[2])
-		raw_file = file.read()
+		raw_file = file.read().strip()
 		contents = raw_file.split("\n")
+		# print(*contents, sep = ", ")
 		file.close()
 	else:
 		print("ERROR: Usage p1.py [mode] [input]")
 		sys.exit();
 
 	# initialize variables
-	cycles_header = "{:<20} {:<4} {:<4} {:<4} {:<4} {:<4} {:<4} {:<4} {:<4} {:<4} {:<4} {:<4} {:<4} {:<4} {:<4} {:<4} {:<4}".format("CPU Cycles ===>", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
-	inst = Instruction()
+	cycles_header = "{:<20}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}".format("CPU Cycles ===>", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+	instructions = []
+	s0 = s1 = s2 = s3 = s4 = s5 = s6 = s7 = 0
+	t0 = t1 = t2 = t3 = t4 = t5 = t6 = t7 = t8 = t9 = 0
+
+	for inst in contents:
+		instructions.append(Instruction(inst, 0, 0, False))
 
 	# Print start
 	print("START OF SIMULATION ", end = "")
@@ -37,14 +43,20 @@ if __name__ == "__main__":
 
 	# Go through the cycles
 	for i in range(0,16):
-		print("-" * 98)
+		print("-" * 82)
 		print(cycles_header)
-		print(inst)
+		for x in instructions:
+			print(x)
 		print()
+		print("$s0 = {:<14}$s1 = {:<14}$s2 = {:<14}$s3 = {:<14}".format(s0, s1, s2, s3))
+		print("$s4 = {:<14}$s5 = {:<14}$s6 = {:<14}$s7 = {:<14}".format(s4, s5, s6, s7))
+		print("$t0 = {:<14}$t1 = {:<14}$t2 = {:<14}$t3 = {:<14}".format(t0, t1, t2, t3))
+		print("$t4 = {:<14}$t5 = {:<14}$t6 = {:<14}$t7 = {:<14}".format(t4, t5, t6, t7))
+		print("$t8 = {:<14}$t9 = {:<14}".format(t8, t9))
 
 
 	# End of the simulation
-	print("-" * 98)
+	print("-" * 82)
 	print("END OF SIMULATION")
 
 	# print("Contents: ", contents)
