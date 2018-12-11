29212fa5695ed0201d6c53a68dc91195eea4aae5
saprap1 <priyasapra.2017@gmail.com>
2018-12-10 23:54:24 -0500

    assign registers and end loop when needed

--- p1.py
+++ p1.py
@@ -7,6 +7,14 @@
 import sys
 from Instruction import Instruction
 
+def assignRegisters(i, registers):
+	split_i = i.mips.split(" ")
+	split_i = split_i[1].split(",")
+	target_reg = split_i[0]
+	val = split_i[-1]
+	if val.isdigit():
+		registers[target_reg] = int(val)
+
 if __name__ == "__main__":
 	if len(sys.argv) == 3:
 		# assign mode - F for Forwarding and N for Non-forwarding
@@ -22,10 +30,16 @@ if __name__ == "__main__":
 		sys.exit();
 
 	# initialize variables
-	cycles_header = "{:<20}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}".format("CPU Cycles ===>", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+	cycles_header = "{:<20}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}"\
+					.format("CPU Cycles ===>", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
 	instructions = []
-	s0 = s1 = s2 = s3 = s4 = s5 = s6 = s7 = 0
-	t0 = t1 = t2 = t3 = t4 = t5 = t6 = t7 = t8 = t9 = 0
+	registers = {'$s0': 0, '$s1': 0, '$s2': 0, '$s3': 0, 
+				 '$s4': 0, '$s5': 0, '$s6': 0, '$s7': 0,
+				 '$t0': 0, '$t1': 0, '$t2': 0, '$t3': 0,
+				 '$t4': 0, '$t5': 0, '$t6': 0, '$t7': 0,
+				 '$t8': 0, '$t9': 0}
+	# s0 = s1 = s2 = s3 = s4 = s5 = s6 = s7 = 0
+	# t0 = t1 = t2 = t3 = t4 = t5 = t6 = t7 = t8 = t9 = 0
 	start_cycle = 0	# This is to know at one cycle the instruction has started
 
 	for inst in contents:
@@ -42,26 +56,34 @@ if __name__ == "__main__":
 
 	# Go through the cycles (cc = clock cycle)
 	for cc in range(0,16):
+
+      	# If the last instruction is done, we stop printing entirely
+		if instructions[-1].complete:
+			break
+		
 		print("-" * 82)
 		print(cycles_header)
 
 		for x in instructions:
 			# Advance stage if needed (the check is done in the function)
 			x.advanceStage(cc)
+            
+			if x.complete:
+				assignRegisters(x, registers)
 
 			# Only print instruction if instruction has started
 			if (cc >= x.start):
 				print(x)
-		
+
+
 		print()
-		print("$s0 = {:<14}$s1 = {:<14}$s2 = {:<14}$s3 = {:<14}".format(s0, s1, s2, s3))
-		print("$s4 = {:<14}$s5 = {:<14}$s6 = {:<14}$s7 = {:<14}".format(s4, s5, s6, s7))
-		print("$t0 = {:<14}$t1 = {:<14}$t2 = {:<14}$t3 = {:<14}".format(t0, t1, t2, t3))
-		print("$t4 = {:<14}$t5 = {:<14}$t6 = {:<14}$t7 = {:<14}".format(t4, t5, t6, t7))
-		print("$t8 = {:<14}$t9 = {:<14}".format(t8, t9))
+		print("$s0 = {:<14}$s1 = {:<14}$s2 = {:<14}$s3 = {:<14}".format(registers["$s0"], registers["$s1"], registers["$s2"], registers["$s3"]))
+		print("$s4 = {:<14}$s5 = {:<14}$s6 = {:<14}$s7 = {:<14}".format(registers["$s4"], registers["$s5"], registers["$s6"], registers["$s7"]))
+		print("$t0 = {:<14}$t1 = {:<14}$t2 = {:<14}$t3 = {:<14}".format(registers["$t0"], registers["$t1"], registers["$t2"], registers["$t3"]))
+		print("$t4 = {:<14}$t5 = {:<14}$t6 = {:<14}$t7 = {:<14}".format(registers["$t4"], registers["$t5"], registers["$t6"], registers["$t7"]))
+		print("$t8 = {:<14}$t9 = {:<14}".format(registers["$t8"], registers["$t9"]))
 
 
 	# End of the simulation
 	print("-" * 82)
 	print("END OF SIMULATION")
-
