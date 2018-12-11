ba039de730c7fb0c9be38fe9b55d48ad986fdf66
Justin Gaskins <gaskij@rpi.edu>
2018-12-11 02:00:43 -0500

    Removed trailing whitespace: 65/100 on Submitty

--- Instruction.py
+++ Instruction.py
@@ -1,5 +1,5 @@
 class Instruction(object):
-    
+
     def __init__( self, mips="", stage=0, stalls=0, start=0, complete=False):
         self.mips = mips            # actual MIPS instruction
         self.stage = stage          # current stage (0-5 for -, ID, IF, EX, MEM, WB)
@@ -9,14 +9,14 @@ class Instruction(object):
         self.cycles = ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
 
     def __str__(self):
-    	return ("{:<20}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}").format(
+    	return ("{:<20}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{:<4}{}").format(
     			self.mips, self.cycles[0], self.cycles[1], self.cycles[2], self.cycles[3],
     			self.cycles[4], self.cycles[5], self.cycles[6], self.cycles[7],
                 self.cycles[8], self.cycles[9], self.cycles[10], self.cycles[11],
                 self.cycles[12], self.cycles[13], self.cycles[14], self.cycles[15])
-    
+
     # add to the current stage
-    def advanceStage(self, cycle): 
+    def advanceStage(self, cycle):
         stage_names = [".", "IF", "ID", "EX", "MEM", "WB"]
         # If the instruction should be started or has been started, add 1 to the stage
         if (cycle >= self.start) and (self.complete == False):
@@ -29,4 +29,3 @@ class Instruction(object):
             self.complete = True
         # set the clock cycle stage
         self.cycles[cycle] = stage_names[self.stage]
-
